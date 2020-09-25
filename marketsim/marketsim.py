42ce1a4e9ef4cd33397cdadf87f8fd30ab9074a3
Thomas Kim <tkim338@gatech.edu>
2020-09-25 17:41:43 -0400

    working code proj 5

--- marketsim/marketsim.py
+++ marketsim/marketsim.py
@@ -35,7 +35,10 @@ import pandas as pd
 from util import get_data, plot_data  		  	   		     		  		  		    	 		 		   		 		  
   		  	   		     		  		  		    	 		 		   		 		  
   		  	   		     		  		  		    	 		 		   		 		  
-def compute_portvals(  		  	   		     		  		  		    	 		 		   		 		  
+def author():
+    return 'tkim338'
+
+def compute_portvals(
     orders_file="./orders/orders.csv",  		  	   		     		  		  		    	 		 		   		 		  
     start_val=1000000,  		  	   		     		  		  		    	 		 		   		 		  
     commission=9.95,  		  	   		     		  		  		    	 		 		   		 		  
@@ -58,18 +61,91 @@ def compute_portvals(
     # this is the function the autograder will call to test your code  		  	   		     		  		  		    	 		 		   		 		  
     # NOTE: orders_file may be a string, or it may be a file object. Your  		  	   		     		  		  		    	 		 		   		 		  
     # code should work correctly with either input  		  	   		     		  		  		    	 		 		   		 		  
-    # TODO: Your code here  		  	   		     		  		  		    	 		 		   		 		  
-  		  	   		     		  		  		    	 		 		   		 		  
-    # In the template, instead of computing the value of the portfolio, we just  		  	   		     		  		  		    	 		 		   		 		  
-    # read in the value of IBM over 6 months  		  	   		     		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2008, 1, 1)  		  	   		     		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2008, 6, 1)  		  	   		     		  		  		    	 		 		   		 		  
-    portvals = get_data(["IBM"], pd.date_range(start_date, end_date))  		  	   		     		  		  		    	 		 		   		 		  
-    portvals = portvals[["IBM"]]  # remove SPY  		  	   		     		  		  		    	 		 		   		 		  
-    rv = pd.DataFrame(index=portvals.index, data=portvals.values)  		  	   		     		  		  		    	 		 		   		 		  
-  		  	   		     		  		  		    	 		 		   		 		  
-    return rv  		  	   		     		  		  		    	 		 		   		 		  
-    return portvals  		  	   		     		  		  		    	 		 		   		 		  
+    # TODO: Your code here
+
+    orders_df = pd.read_csv(orders_file)
+
+    start_date = dt.datetime.strptime(orders_df["Date"].min(), '%Y-%m-%d')
+    end_date = dt.datetime.strptime(orders_df["Date"].max(), '%Y-%m-%d')
+    day_increment = dt.timedelta(days=1)
+    all_dates = []
+    date_i = start_date
+    while date_i <= end_date:
+        all_dates.append(date_i)
+        date_i += day_increment
+
+    portvals = get_data(['$SPX'], pd.date_range(start_date, end_date), addSPY=False)
+    holdings = {'cash': start_val}
+
+    retrieved_data = {}
+
+    def update_holdings(date):
+        curr_orders = orders_df.loc[orders_df['Date'] == date.strftime('%Y-%m-%d')]
+        for index, row in curr_orders.iterrows():
+            sym_val = get_data([row['Symbol']], [date], addSPY=False)[row['Symbol']].iloc[0]
+            holdings['cash'] -= commission
+
+            if row['Symbol'] in holdings:
+                if row['Order'] == 'BUY':
+                    holdings[row['Symbol']] += row['Shares']
+                    holdings['cash'] -= row['Shares'] * sym_val * (1+impact)
+                else:  # sell
+                    holdings[row['Symbol']] -= row['Shares']
+                    holdings['cash'] += row['Shares'] * sym_val * (1-impact)
+            else:
+                if row['Order'] == 'BUY':
+                    holdings[row['Symbol']] = row['Shares']
+                    holdings['cash'] -= row['Shares'] * sym_val * (1+impact)
+                else:  # sell
+                    holdings[row['Symbol']] = -row['Shares']
+                    holdings['cash'] += row['Shares'] * sym_val * (1-impact)
+        return holdings
+
+    def get_portfolio_value(curr_holdings, date):
+        pv = 0
+        for key in holdings:
+            if key == 'cash':
+                pv += curr_holdings[key]
+            else:
+                if key not in retrieved_data:
+                    retrieved_data[key] = get_data([key], all_dates, addSPY=False)
+                sym_val = retrieved_data[key][key][date]
+                pv += sym_val*holdings[key]
+        return pv
+
+    curr_date = start_date
+
+    while curr_date <= end_date:
+        spx_df = get_data(['$SPX'], [curr_date], addSPY=False)
+        if not np.isnan(spx_df['$SPX'].iloc[0]):
+            # update holdings based on today's orders
+            holdings = update_holdings(curr_date)
+
+            # compute today's portfolio value
+            curr_pv = get_portfolio_value(holdings, curr_date)
+
+            # update portfolio value with today's value
+            portvals['$SPX'][curr_date] = curr_pv
+        else:
+            portvals = portvals.drop(curr_date)
+
+        # go to next trading day
+        curr_date += day_increment
+
+    # In the template, instead of computing the value of the portfolio, we just
+    # read in the value of IBM over 6 months
+    # start_date = dt.datetime(2008, 1, 1)
+    # end_date = dt.datetime(2008, 6, 1)
+    # portvals = get_data(["IBM"], pd.date_range(start_date, end_date))
+    # portvals = portvals[["IBM"]]  # remove SPY
+    # rv = pd.DataFrame(index=portvals.index, data=portvals.values)
+    #
+    # return rv
+    # return portvals
+
+
+    rv = pd.DataFrame(index=portvals.index, data=portvals.values)
+    return rv
   		  	   		     		  		  		    	 		 		   		 		  
   		  	   		     		  		  		    	 		 		   		 		  
 def test_code():  		  	   		     		  		  		    	 		 		   		 		  
@@ -80,7 +156,7 @@ def test_code():
     # note that during autograding his function will not be called.  		  	   		     		  		  		    	 		 		   		 		  
     # Define input parameters  		  	   		     		  		  		    	 		 		   		 		  
   		  	   		     		  		  		    	 		 		   		 		  
-    of = "./orders/orders2.csv"  		  	   		     		  		  		    	 		 		   		 		  
+    of = "./orders/orders.csv"
     sv = 1000000  		  	   		     		  		  		    	 		 		   		 		  
   		  	   		     		  		  		    	 		 		   		 		  
     # Process orders  		  	   		     		  		  		    	 		 		   		 		  
@@ -126,4 +202,49 @@ def test_code():
   		  	   		     		  		  		    	 		 		   		 		  
   		  	   		     		  		  		    	 		 		   		 		  
 if __name__ == "__main__":  		  	   		     		  		  		    	 		 		   		 		  
-    test_code()  		  	   		     		  		  		    	 		 		   		 		  
+    # test_code()
+
+    # symbols = ["AAPL"]
+    # dates = [dt.datetime(2011, 1, 10), dt.datetime(2012, 1, 10), ]
+    # dat = get_data(symbols, dates)
+    # print(dat)
+
+    # d1 = dt.datetime(2012, 1, 1)
+    # d2 = d1 + dt.timedelta(days=1)
+    #
+    # print(d1)
+    # print(d2)
+
+    # df = get_data(['$SPX'], [dt.datetime(2012, 8, 27), dt.datetime(2012, 8, 24), dt.datetime(2012, 8, 25)], addSPY=False)
+    # print(df)
+
+    of = "./orders/orders2.csv"
+    sv = 1000000
+    portvals = compute_portvals(orders_file=of, start_val=sv)
+    print(portvals[portvals.columns[0]][-1])
+
+    def get_port_value(allocations):
+        portfolio_value = 0 * prices_all["SPY"]
+        for i in range(len(syms)):
+            curr_sym = syms[i]
+            curr_allocation = allocations[i]
+            for t in range(len(prices_all)):
+                portfolio_value[t] += prices_all[curr_sym][t]/prices_all[curr_sym][0] * curr_allocation
+        return portfolio_value
+
+    def get_port_return_daily(allocations):
+        portfolio_return = 0 * prices_all["SPY"]
+        for i in range(len(syms)):
+            curr_sym = syms[i]
+            curr_allocation = allocations[i]
+            for t in range(1, len(prices_all)):
+                portfolio_return[t] += ((prices_all[curr_sym][t]/prices_all[curr_sym][t-1]) - 1) * curr_allocation
+        return portfolio_return
+
+    def compute_sharpe_ratio(allocations):
+        R_b = 0  # risk-free return
+        port_history = get_port_return_daily(allocations)[1:]
+        sigma_a = np.std(port_history)
+        R_a = port_history
+        S_a = np.mean(R_a - R_b) / sigma_a
+        return S_a * np.sqrt(252)
